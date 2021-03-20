e73bef28a6bc6d19029761a2c6b74b017dc3aff1
Kimberly Gonzalez <ksgonzalez95@gmail.com>
2021-03-20 19:55:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-public class Permutation 
-{ 
-    public static void main(String[]) 
-    { 
-        String str = "123"; 
-        int n = str.length(); 
-        Permutation permutation = new Permutation(); 
-        permutation.permute(str, 0, n-1); 
-    } 
+public class Permutation
+{
+    public static void main(String[])
+    {
+        String str = "123";
+        int n = str.length();
+        Permutation permutation = new Permutation();
+        permutation.permute(str, 0, n-1);
+    }
 
 
 findAllPermutations('123')
